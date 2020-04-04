1d1173eece768dafff06dafbb39605193c9f7cd7
Andy <aw087ok@gmail.com>
2020-04-04 17:52:50 +0100

    Performs audio extraction and .wav file outputting once

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 
 import pyaudio
 import sys
+import wave
 
 """ Retrieving the USB audio device ID to perform the audio extraction """
 
@@ -21,6 +22,7 @@ deviceCount = info.get('deviceCount')
 inputDeviceId = -1
 print("Retrieving USB Audio Input Device ID..")
 
+# Find the current USB audio device to use
 for id in range(0, deviceCount):
   if (p.get_device_info_by_host_api_device_index(0, id).get('maxInputChannels')) > 0:
     inputDeviceId = id
@@ -35,3 +37,50 @@ if inputDeviceId == -1:
   sys.exit()
 else:
   print("Using Input Device ID $d" % inputDeviceId)
+
+""" Extracting audio operation using the USB audio device found """
+
+# Constants
+RESPEAKER_RATE = 16000
+RESPEAKER_CHANNELS = 6 # change base on firmwares, 1_channel_firmware.bin as 1 or 6_channels_firmware.bin as 6
+RESPEAKER_WIDTH = 2
+CHUNK = 1024
+RECORD_FOR = 5 # in seconds
+WAVE_OUTPUT_FILENAME = "output/output.wav"
+
+audioLength = int(RESPEAKER_RATE / CHUNK * RECORD_FOR)
+audioFrames = []
+
+# Stream for listening to audio
+stream = p.open(
+  rate = RESPEAKER_RATE,
+  format = p.get_format_from_width(RESPEAKER_WIDTH),
+  channels = RESPEAKER_CHANNELS,
+  input = True,
+  input_device_index = inputDeviceId)
+
+print("Listening to audio..")
+
+# Listen to audio
+for i in range(0, audioLength):
+  audioStream = stream.read(CHUNK)
+  audioFrames.append(audioStream)
+
+print("fin. Extracting audio..")
+
+# fin. with extraction; close all streams
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+print("Outputting audio to file..")
+
+# Output to file appropriately
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(RESPEAKER_CHANNELS)
+wf.setsampwidth(p.get_sample_size(p.get_format_from_width(RESPEAKER_WIDTH)))
+wf.setframerate(RESPEAKER_RATE)
+wf.writeframes(b''.join(audioFrames))
+wf.close()
+
+print("fin. Outputting audio to file.")
